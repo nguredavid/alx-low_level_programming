2a5764f831a18a9ea0176fa1e4ad92010cca9073
root <daviengush@gmail.com>
2022-04-07 11:01:36 +0000

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - entry point
  *
  * Return:0 after printing the function
- * /
+ */
 int main(void)
 {
-       	puts("Programming is like building a multilingual puzzle\n");
+       	puts("\"Programming is like building a multilingual puzzle");
        	return (0);
 }
