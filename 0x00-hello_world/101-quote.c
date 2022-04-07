422ae04d8d5650174ef91b2512018a6d4f25cced
root <daviengush@gmail.com>
2022-04-07 20:33:27 +0000

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ * @ngure - value declared
+ * Return: returning 1 at execution
+ */
+int main(void)
+{
+	char ngure = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(ngure);
+
+	return (1);
+}
