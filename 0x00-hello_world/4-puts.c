d7168278bba5d8d1394e43615fdb91d7d9cb688f
root <daviengush@gmail.com>
2022-04-07 10:58:14 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
-
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+/**
+ * main - entry point
+ *
+ * Return:0 after printing the function
+ * /
+int main(void)
+{
+       	puts("Programming is like building a multilingual puzzle\n");
+       	return (0);
+}
