9c84b79b5af04a4783a782298221b7f05aa3e95a
root <daviengush@gmail.com>
2022-04-07 14:28:00 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *
- * main function print
- *
- * return 0
+ *main - entry point
+ *my hello, printf
+ *return: return t after execution
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
 
