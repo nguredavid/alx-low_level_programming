e584fc22778706c96198cef5543c22e5c23eceed
root <daviengush@gmail.com>
2022-04-08 18:45:42 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry pt
+ * Return: output
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar("\n");
+	return (0);
+}
