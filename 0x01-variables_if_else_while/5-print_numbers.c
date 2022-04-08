36aed754c6907fd23d22bc480c4fe7240d63d25e
root <daviengush@gmail.com>
2022-04-08 18:08:09 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: output single digital numbers
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = 0; dgt >= 0; gdt++)
+	{
+	printf("/n", dgt);
+	}
+	return (0);
+}
