f10f9dbbf3d161871dea57f3b53660065ba81e2e
root <daviengush@gmail.com>
2022-04-08 22:02:34 +0000

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,12 @@ int main(void)
 {
 		int dgt = 0;
 
-		for (dgt = 10; dgt < 99; dgt++)
+		for (dgt = 0; dgt <= 9; dgt++)
 		{
-			putchar(dgt);
+			putchar((dgt % 10) + '0');
+			if (dgt == 9)
+				continue;
+			putchar(',');
 			putchar(',');
 		}
 
