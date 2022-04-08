722ff16b7d0ab34f983c0f0d77b204584883dedb
root <daviengush@gmail.com>
2022-04-08 17:07:21 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: output results
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp = 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	for (alp = 'A'; alp = 'Z'; alp++)
+
+	{
+		putchar(alp);
+	}
+		putchar('\n');
+
+		Return(0);
+}
