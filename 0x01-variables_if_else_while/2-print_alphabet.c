56da491858cb1024d5c14ac54a7e81afe0c79cd6
root <daviengush@gmail.com>
2022-04-08 13:39:46 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	char = alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; apha++;)
+	{
+	putchar(apha);
+	}
+	putchar('\n')
+
+	return (0);
+}
