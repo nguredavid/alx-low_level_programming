fc0cd4605f19af4b7afd57e7235523dabced501f
root <daviengush@gmail.com>
2022-04-08 17:34:21 +0000

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry pt
+ * Return: output the results
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'e')
+		continue;
+		else if (alph == 'q')
+		continue;
+	{
+	putchar(alph);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
