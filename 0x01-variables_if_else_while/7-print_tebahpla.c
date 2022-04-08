cfdfe8326f0e17c8e01324e398fdc4deaaa1326e
root <daviengush@gmail.com>
2022-04-08 21:06:17 +0000

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,6 +11,6 @@ int main(void)
 	{
 		putchar(rev);
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  * main - entry pt
+ *   * Return: reverse output
+ *    */
+int main(void)
+{
+	        char rev;
+
+		 for (rev = 'z'; rev >= 'a'; rev--) 
+		 {
+		 putchar(rev);
+		 }
+	 	putchar('\n');
+	 	return (0);
+}
