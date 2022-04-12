ff9604eaf7e66da3a1c55c249989ae06acc2d99f
root <daviengush@gmail.com>
2022-04-12 17:36:30 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 	int hr;
 	int min;
 
-	for (hr = 00; hr < 24; hr++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (min = 00; min < 60; min++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
-			_putchar(min);
+			_putchar(min % 10) + '0';
 			_putchar('\n');
 		}
 	}
