dc966957d424a7c57528dffdfdd1d7ae0b24e96a
root <daviengush@gmail.com>
2022-04-12 13:39:43 +0000

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- *
+ * print_alphabet_x10 - entry pt
+ * Return: always o
  */
 void print_alphabet_x10(void);
 {
 	char alp;
 	int a;
+
 	for (a = 1; a < 10; a++)
 	{
 		for (alp = 'a'; alp <= 'z'; alp++)
