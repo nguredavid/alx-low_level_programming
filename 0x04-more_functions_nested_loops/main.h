a66e08b5b6a9cefc91b171e75225ab4de340dd13
root <daviengush@gmail.com>
2022-04-14 21:54:08 +0000

    task 10

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,6 +14,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
