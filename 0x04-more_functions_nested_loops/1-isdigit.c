61d87353bab4775fecafdd4ac882aa36f338b88e
root <daviengush@gmail.com>
2022-04-14 16:44:38 +0000

    add readme

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * @c: variable
+ * Return: always 1
+ */
+int _isdigit(int c);
+{
+	int c;
+
+		if ((c >= 0) && (c <= 9))
+
+			return (1);
+
+		else
+		return (0);
+}
+
+
