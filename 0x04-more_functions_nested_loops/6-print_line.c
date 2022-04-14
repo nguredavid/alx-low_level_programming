e329b0df6daec4c764816df0fa87a76c92213cab
root <daviengush@gmail.com>
2022-04-14 20:05:11 +0000

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,14 @@ void print_line(int n)
 {
 	int a = 0;
 
-		if (n > 0)
+	if (n > 0)
+	{
+		while (a <= n)
 		{
-			while (a <= n)
-			{
-				_putchar('_');
-				a++;
-			}
+			_putchar('_');
+			a++;
+
+		}
 		 _putchar('\n');
 		}
 		else
