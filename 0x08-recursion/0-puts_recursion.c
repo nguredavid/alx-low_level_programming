c254f8d1b21f1327666796d38c3cfa7bdbfb7a50
root <daviengush@gmail.com>
2022-04-28 12:01:34 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - supposed to print a spring
+ * @s: source string
+ * Return: return printed string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
