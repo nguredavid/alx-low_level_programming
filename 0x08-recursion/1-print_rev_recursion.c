013779f964dcb863de9e834c4e2432b1865b0740
root <daviengush@gmail.com>
2022-04-28 15:18:29 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * void _print_rev_recursion(char *s) - entry pt
+ * @s: input
+ * Return: Return 0 success
+ */
+void _print_rev_recursion(char *s)
+{
+		if (*s != '\0')
+
+			 _print_rev_recursion(s + 1);
+			 
+		else 
+			return;
+		_putchar(*s);
+}
+
