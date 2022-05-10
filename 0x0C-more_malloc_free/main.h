7732e6262b694de48f733eaa57800e17e5ffeb43
root <daviengush@gmail.com>
2022-05-10 18:37:59 +0000

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- * used in the 0x0B-more_malloc_free directory.
- */
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
