e8a3f43c28ebaff1a2a91b3475a77c2de1bf1ac6
nguredavid <daviengush@gmail.com>
2022-05-24 13:41:01 +0000

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,17 @@
-global main
+section .data
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
 
 	section .text
+	extern printf
+	global main
+
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
-	syscall
-
-	mov eax, 60
-	xor rdi, rdi
-	syscall
-
-message:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
