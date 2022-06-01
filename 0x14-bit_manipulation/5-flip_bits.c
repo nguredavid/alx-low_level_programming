21f3475f01c78fda9d2bca5324459843eb6f2fcc
David <64853005+nguredavid@users.noreply.github.com>
2022-06-01 13:00:36 +0300

    Update 5-flip_bits.c
    fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,24 +1,26 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-  * flip_bits - ...
-  * @n: ...
-  * @m: ...
-  *
-  * Return: ...
-  */
+ * flip_bits - number of different bits between two numbers
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits you would need to flip
+ * to get from one number to another.
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int mask = 0, count = 0;
+	unsigned long int diff, check;
+	unsigned int count, i;
 
-	mask = n ^ m;
-	while (mask)
+	count = 0;
+	check = 1;
+	diff = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
-		if (mask & 1)
+		if (check == (diff & check))
 			count++;
-
-		mask >>= 1;
+		check <<= 1;
 	}
-
 	return (count);
+}
