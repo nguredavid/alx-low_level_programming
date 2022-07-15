a26552be288394f7becb52d2c91049e1babff798
David <64853005+nguredavid@users.noreply.github.com>
2022-07-15 21:51:02 +0300

    Update main.h
    FIX

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,12 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stdlib.h>
+#ifndef MAIN
+#define MAIN
 
-/**
- * Structs, enums and unions definitions
- * Typedefs
- * Function prototypes
- */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,7 +15,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -29,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HOLBERTON */
